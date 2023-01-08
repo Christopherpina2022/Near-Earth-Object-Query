4589b275d3cfcd545b4a6fa95213d8d5fd6dc4c5
chriscitric <chriscitric@gmail.com>
2023-01-08 14:59:27 -0600

    Data Dictionary rewrite

--- DictionaryInit.py
+++ DictionaryInit.py
@@ -1,14 +1,10 @@
-import json
+import requests
 import urllib3
 import os
-from dotenv import load_dotenv
-
-# Creates the Data Dictionary that is called throughout the program.
 
 # Create a get request for the browse feature in the NASA NeoWs API
-load_dotenv()
 http = urllib3.PoolManager()
-NeoBrowse = http.request('GET', "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=" + os.getenv('API_KEY'))
+neoBrowse = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key='+ os.getenv('API_KEY'))
 
-# Define a data Dictionary with decrypted JSON
-CE_dict = json.loads(NeoBrowse.data.decode('UTF-8'))
+# Define a data Dictionary with parsed information
+CE_dict = neoBrowse.json()
