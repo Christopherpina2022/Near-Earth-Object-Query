4a964aed2d6060869d46ce574292d17301b69c87
Chris Pina <chris.pina@kochind.com>
2023-03-09 10:52:59 -0600

    Updating UI

--- Menu.py
+++ Menu.py
@@ -46,8 +46,16 @@ combo = ttk.Combobox(top, values= listItems).pack()
 #These widgets disappear after an item is selected and reappear after nothing is selected
 minAvgDiameter = Label(top, text="Average Min Diameter: " + str(sizeMin)).pack()
 maxAvgDiameter = Label(top, text="Average Max Diameter: " + str(sizeMax)).pack()
-#if combo.get() == '':
-#    minAvgDiameter
+
+# Creating all the data i want to pull per request, data will by default be hidden
+minDiameter = Label(top, text="Min Diameter (meters): " + str(sizeMin)).pack()
+maxDiameter = Label(top, text="Max Diameter (meters): " + str(sizeMax)).pack()
+firstObservation = Label(top, text="First observed in : " + str(sizeMax)).pack()
+neoMagnitude = Label(top, text="Magnitude: " +str(sizeMax)).pack()
+nextApproachDate = Label(top, text="Next approach Date: " + str(sizeMax)).pack()
+nearestMiss = Label(top, text="Nearest Miss: " + str(sizeMax) + " by "+ str(sizeMax) + " Kilometers").pack() # Date and then distance
+
+
 
 # I found that you can create a default Canvas of Grids, could be useful, might include it for graphs.
 
